ec95fa3e09a436fc039f82c1bbfed1fbe74d0421
Rafael Gomes <rafarfl2010@gmail.com>
2020-05-12 14:34:20 -0300

    update title pages

---
+++ crud/frontend/src/app/components/templates/header/header.service.ts
@@ -0,0 +1,26 @@
+import { HeaderData } from './header-data.model';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeaderService {
+
+  private _headerData = new BehaviorSubject<HeaderData>({
+    title: 'Início 2',
+    icon: 'home',
+    routeUrl: ''
+  })
+
+  constructor() { }
+
+  get headerData(): HeaderData {
+    return this._headerData.value;
+  }
+
+  set HeaderData(headerDate: HeaderData) {
+    this._headerData.next(headerDate)
+  }
+
+}
